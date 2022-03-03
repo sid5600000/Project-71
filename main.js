504a2ab27c4e7199ee6b4459c86893acd1404b39
sid5600000 <86678953+sid5600000@users.noreply.github.com>
2022-03-03 19:59:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var first_name = "King ";
+var second_name =  "Kohli";
+var full_name = first_name.concat(second_name);
+console.log("The full name and the ruler of cricket is : "+ full_name);
+
+var a = 4;
+var b = 8;
+
+var sum = a + b;
+var dif = b - a;
+var prod = a * b;
+var quo = b / a;
+var rem = b % a;
+
+console.log("A = " + a);
+console.log("B = " + b);
+console.log("The sum of these numbers is : " + sum);
+console.log("The difference of these numbers is : " + dif);
+console.log("The product of these numbers is : " + prod);
+console.log("The quotient of these numbers is : " + quo);
+console.log("The remainder of these numbers is : " + rem);
